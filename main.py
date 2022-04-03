0575b4c874b28b2e9bb53030e683617eeb336b35
krook98 <sokalski.jakub98@gmail.com>
2022-04-03 23:31:43 +0200

    EDIT post

--- main.py
+++ main.py
@@ -62,9 +62,25 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/edit_post")
-def edit_post():
-    pass
+@app.route("/edit_post/<post_id>", methods=['GET', 'POST'])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for('show_post', post_id=post_id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
 
 
 @app.route("/new_post", methods=['GET','POST'])
